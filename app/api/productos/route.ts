8d502dbebad092e59c21969cacbf5ad74a0e5291
MarAlfaro <alfaromargarita556@gmail.com>
2023-12-10 20:51:48 -0600

    Create route.ts

---
+++ app/api/productos/route.ts
@@ -0,0 +1,31 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+
+export async function POST(
+    req: Request,
+) {
+    try {
+        const { userId } = auth();
+        const dataValues = await req.json();
+
+        if (!userId) {
+            return new NextResponse("No autorizado", { status: 401 });
+        }
+
+        const course = await db.tbl_products.create({
+            data: {
+                id_usuario: userId,
+                ...dataValues
+            }
+        });
+
+        console.log(course);
+
+        return NextResponse.json(course);
+    } catch (error) {
+        console.log("[CURSOS]", error);
+        return new NextResponse("Error Interno", { status: 500 });
+    }
+}
